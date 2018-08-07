0297a3171bee7b19566d7fa7ac564f54c63497de
Pujan Bajracharya <pujanisme@gmail.com>
2018-08-07 17:53:52 +0545

    Initial Commit

---
+++ Engine/LivingCreature.cs
@@ -0,0 +1,14 @@
+﻿namespace Engine
+{
+    public class LivingCreature
+    {
+        public int MaximumHitPoints { get; set; }
+        public int CurrentHitPoints { get; set; }
+
+        public LivingCreature(int maximumHitPoints, int currentHitPoints)
+        {
+            MaximumHitPoints = maximumHitPoints;
+            CurrentHitPoints = currentHitPoints;
+        }
+    }
+}
